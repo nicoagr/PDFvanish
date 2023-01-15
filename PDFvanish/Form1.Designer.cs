d9081e2dca631b59a9eb018a8f353c6b645c61f6
nicoagr <github@nico.eus>
2023-01-15 18:34:12 +0100

    Agregar archivos de proyecto.

---
+++ PDFvanish/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace PDFvanish
+{
+    partial class PDFvanish
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileList = new System.Windows.Forms.ListBox();
+            this.actionBtn = new System.Windows.Forms.Button();
+            this.selectFileBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.clearListBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fileList
+            // 
+            this.fileList.AllowDrop = true;
+            this.fileList.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileList.FormattingEnabled = true;
+            this.fileList.ItemHeight = 31;
+            this.fileList.Items.AddRange(new object[] {
+            "Drag & Drop Items Here"});
+            this.fileList.Location = new System.Drawing.Point(17, 85);
+            this.fileList.Name = "fileList";
+            this.fileList.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.fileList.Size = new System.Drawing.Size(800, 252);
+            this.fileList.TabIndex = 0;
+            this.fileList.DragDrop += new System.Windows.Forms.DragEventHandler(this.fileList_DragDrop);
+            this.fileList.DragEnter += new System.Windows.Forms.DragEventHandler(this.fileList_DragEnter);
+            this.fileList.DoubleClick += new System.EventHandler(this.fileList_DoubleClick);
+            // 
+            // actionBtn
+            // 
+            this.actionBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.actionBtn.Location = new System.Drawing.Point(301, 371);
+            this.actionBtn.Name = "actionBtn";
+            this.actionBtn.Size = new System.Drawing.Size(253, 50);
+            this.actionBtn.TabIndex = 1;
+            this.actionBtn.Text = "Remove Metadata";
+            this.actionBtn.UseVisualStyleBackColor = true;
+            this.actionBtn.Click += new System.EventHandler(this.actionBtn_Click);
+            // 
+            // selectFileBtn
+            // 
+            this.selectFileBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectFileBtn.Location = new System.Drawing.Point(561, 49);
+            this.selectFileBtn.Name = "selectFileBtn";
+            this.selectFileBtn.Size = new System.Drawing.Size(125, 30);
+            this.selectFileBtn.TabIndex = 2;
+            this.selectFileBtn.Text = "Select File...";
+            this.selectFileBtn.UseVisualStyleBackColor = true;
+            this.selectFileBtn.Click += new System.EventHandler(this.selectFileBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(476, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "PDFvanish - Remove all metadata from your PDF files";
+            // 
+            // clearListBtn
+            // 
+            this.clearListBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearListBtn.Location = new System.Drawing.Point(692, 49);
+            this.clearListBtn.Name = "clearListBtn";
+            this.clearListBtn.Size = new System.Drawing.Size(125, 30);
+            this.clearListBtn.TabIndex = 4;
+            this.clearListBtn.Text = "Clear List";
+            this.clearListBtn.UseVisualStyleBackColor = true;
+            this.clearListBtn.Click += new System.EventHandler(this.clearListBtn_Click);
+            // 
+            // PDFvanish
+            // 
+            this.AcceptButton = this.selectFileBtn;
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 453);
+            this.Controls.Add(this.clearListBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.selectFileBtn);
+            this.Controls.Add(this.actionBtn);
+            this.Controls.Add(this.fileList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "PDFvanish";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PDFvanish v1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox fileList;
+        private System.Windows.Forms.Button actionBtn;
+        private System.Windows.Forms.Button selectFileBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button clearListBtn;
+    }
+}
+
---
+++ PDFvanish/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+
+namespace PDFvanish
+{
+    public partial class PDFvanish : Form
+    {
+        public PDFvanish()
+        {
+            InitializeComponent();
+        }
+
+        private void fileList_DragDrop(object sender, DragEventArgs e)
+        {
+            // If we drop a file
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
+                
+                string[] dropped = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (string filepath in dropped)
+                {
+                    if (Path.GetExtension(filepath).Equals(".pdf"))
+                    {
+                        // if textbox empty
+                        if (fileList.Items.Count == 1 && fileList.Items[0].Equals("Drag & Drop Items Here"))
+                        {
+                            fileList.Font = new Font(fileList.Font.FontFamily, 10, FontStyle.Regular);
+                            fileList.Items.Clear();
+                        }
+                        if (!fileList.Items.Contains(filepath)) fileList.Items.Add(filepath);
+                        actionBtn.Focus();
+                        this.AcceptButton = actionBtn;
+                    }
+                }
+            }
+
+        }
+
+        private void actionBtn_Click(object sender, EventArgs e)
+        {
+            ;
+        }
+
+        private void fileList_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void fileList_DoubleClick(object sender, EventArgs e)
+        {
+            //remove item if we double click
+            if (fileList.SelectedItem != null)
+            {
+                fileList.Items.Remove(fileList.SelectedItem);
+                if (fileList.Items.Count == 0)
+                {
+                    fileList.Font = new Font(fileList.Font.FontFamily, (float)16.2, FontStyle.Regular);
+                    fileList.Items.Add("Drag & Drop Items Here");
+                    actionBtn.Focus();
+                    this.AcceptButton = actionBtn;
+                }
+            }
+        }
+
+        private void selectFileBtn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog file = new OpenFileDialog()) {
+                file.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                file.Filter = "PDF Files (*.pdf)|*.pdf";
+                file.Multiselect = true;
+                if (file.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string filepath in file.FileNames)
+                    {
+                        if (Path.GetExtension(filepath).Equals(".pdf"))
+                        {
+                            // if textbox empty
+                            if (fileList.Items.Count == 1 && fileList.Items[0].Equals("Drag & Drop Items Here"))
+                            {
+                                fileList.Font = new Font(fileList.Font.FontFamily, 10, FontStyle.Regular);
+                                fileList.Items.Clear();
+                            }
+                            if (!fileList.Items.Contains(filepath)) fileList.Items.Add(filepath);
+                            actionBtn.Focus();
+                            this.AcceptButton = actionBtn;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void clearListBtn_Click(object sender, EventArgs e)
+        {
+            fileList.Items.Clear();
+            fileList.Font = new Font(fileList.Font.FontFamily, (float)16.2, FontStyle.Regular);
+            fileList.Items.Add("Drag & Drop Items Here");
+            selectFileBtn.Focus();
+            this.AcceptButton = selectFileBtn;
+        }
+    }
+}
